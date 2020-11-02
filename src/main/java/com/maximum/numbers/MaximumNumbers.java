a88bfe64163e33119ee3105641d57280914c0d63
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-02 21:36:24 +0530

    Refactor: Program to find max value with generic class extending comparable

--- src/main/java/com/maximum/numbers/MaximumNumbers.java
+++ src/main/java/com/maximum/numbers/MaximumNumbers.java
@@ -1,13 +1,22 @@
 package com.maximum.numbers;
 
-public class MaximumNumbers {
+public class MaximumNumbers<E extends Comparable<?>> {
+	E num1;
+	E num2;
+	E num3;
 
-	public static <E extends Object> E maximumNumber(E num1, E num2, E num3) {
-		E max = num1;
+	public MaximumNumbers(E num1, E num2, E num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+
+	public <E extends Comparable<?>> E testMaximum() {
+		E max = (E) num1;
 		if (num2.toString().compareTo(max.toString()) > 0)
-			max = num2;
+			max = (E) num2;
 		if (num3.toString().compareTo(max.toString()) > 0)
-			max = num3;
+			max = (E) num3;
 		return max;
 	}
 }
