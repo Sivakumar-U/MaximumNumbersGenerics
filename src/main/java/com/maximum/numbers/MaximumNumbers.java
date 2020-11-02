35cd660f95547470d59c3666395b4800b4546f4a
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-02 15:45:33 +0530

    Added: Given 3 strings to find maximum

--- src/main/java/com/maximum/numbers/MaximumNumbers.java
+++ src/main/java/com/maximum/numbers/MaximumNumbers.java
@@ -21,4 +21,14 @@ public class MaximumNumbers {
 
 	}
 
+	public String maximumNumber(String string1, String string2, String string3) {
+		if (string1.compareTo(string2) > 0 && string1.compareTo(string3) > 0)
+			return string1;
+		else if (string2.compareTo(string3) > 0 && string2.compareTo(string1) > 0)
+			return string2;
+		else
+			return string3;
+
+	}
+
 }
