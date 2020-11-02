959d137171ae7a83922209fa7acfc422825bf634
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-02 15:29:05 +0530

    Added: Given 3 floats to find the maximum

--- src/main/java/com/maximum/numbers/MaximumNumbers.java
+++ src/main/java/com/maximum/numbers/MaximumNumbers.java
@@ -11,4 +11,14 @@ public class MaximumNumbers {
 			return num3;
 	}
 
+	public Float maximumNumber(Float num1, Float num2, Float num3) {
+		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+			return num1;
+		else if (num2.compareTo(num3) > 0 && num2.compareTo(num1) > 0)
+			return num2;
+		else
+			return num3;
+
+	}
+
 }
